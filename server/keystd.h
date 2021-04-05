480dec6ccaec9aa876b326a6bb014dcaec1ee69a
Jacek Leszczyński <sam@bialan.pl>
2021-04-05 09:41:56 +0200

    Dodanie pliku z danymi kluczy.

---
+++ server/keystd.h
@@ -0,0 +1,9 @@
+/*
+  Tu ustawiane są klucze do szyfrowania komunikacji sieciowej.
+  Poniższy kod ma być wypełniony właściwymi kluczami, jednkże z jasnych
+  powodów tych danych nie upubliczniam!
+*/
+
+char *globalny_key = "";
+char *globalny_vec = "";
+
